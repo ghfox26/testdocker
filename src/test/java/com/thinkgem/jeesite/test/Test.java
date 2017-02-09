6a11e3e2551097321f64603f5f96ac7f69593a31
ghfox26 <fox26@126.com>
2017-02-09 14:36:28 +0800

    fix bug

--- src/test/java/com/thinkgem/jeesite/test/Test.java
+++ src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -14,7 +14,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thinkgem.jeesite.common.utils.Encodes;
